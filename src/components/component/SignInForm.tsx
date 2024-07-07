c48dd331116023b278a8675d7b869bb7f0bb10d7
GearHead87 <hosanulislam87@gmail.com>
2024-07-07 23:13:38 +0600

    add: sign in function

--- src/components/component/SignInForm.tsx
+++ src/components/component/SignInForm.tsx
@@ -1,18 +1,42 @@
+"use client"
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { signIn } from 'next-auth/react';
 
 const SignInForm = () => {
+	const formSubmit = async (e) => {
+		e.preventDefault();
+		const form = e.target;
+		const res = await signIn('credentials', {
+			email: form.email.value,
+			password: form.password.value,
+			redirect: true,
+			callbackUrl: '/',
+		});
+	};
 	return (
 		<div>
-			<form className="space-y-4">
+			<form onSubmit={formSubmit} className="space-y-4">
 				<div className="grid gap-2">
 					<Label htmlFor="email">Email</Label>
-					<Input id="email" type="email" placeholder="m@example.com" required />
+					<Input
+						id="email"
+						name="email"
+						type="email"
+						placeholder="m@example.com"
+						required
+					/>
 				</div>
 				<div className="grid gap-2">
 					<Label htmlFor="password">Password</Label>
-					<Input id="password" type="password" required />
+					<Input
+						id="password"
+						name="password"
+						type="password"
+						placeholder="Enter Your Password"
+						required
+					/>
 				</div>
 				<Button type="submit" className="w-full">
 					Register
