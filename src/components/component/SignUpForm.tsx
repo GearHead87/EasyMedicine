536a68cf93d9fe27301b5f69a360bc5d8285ed76
GearHead87 <hosanulislam87@gmail.com>
2024-07-12 09:36:16 +0600

    fix: signup form fix image fix

--- src/components/component/SignUpForm.tsx
+++ src/components/component/SignUpForm.tsx
@@ -62,7 +62,7 @@ export const SignUpForm = () => {
 				</div>
 				<div className="grid w-full max-w-sm items-center gap-1.5">
 					<Label htmlFor="picture">Picture</Label>
-					<Input id="image" name="image" type="file" required />
+					<Input id="image" name="image" type="file" />
 				</div>
 				<Button type="submit" className="w-full">
 					Register
